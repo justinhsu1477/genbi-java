7948d51d30f997ae7ab58563f2e4cad51f300998
hazdik <hazdi.kurniawan@lndata.com>
2026-03-21 00:03:13 +0700

    Initialize Spring Boot project with Maven, basic structure, and dependencies

---
+++ src/main/java/com/lndata/genbi/GenBiInitApplication.java
@@ -0,0 +1,13 @@
+package com.lndata.genbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenBiInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenBiInitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lndata/genbi/GenBiInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lndata.genbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenBiInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
